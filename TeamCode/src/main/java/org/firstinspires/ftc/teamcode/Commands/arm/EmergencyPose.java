4e8de5b2e1162fe54dbc0dfcc0cdb1df20d2767c
CollectingTaxes <cheeseit313@gmail.com>
2024-12-19 14:16:07 -0600

    reverting last commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/arm/EmergencyPose.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.Commands.arm;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Arm;
+
+public class EmergencyPose extends SequentialCommandGroup {
+    public EmergencyPose(Arm arm) {
+        addCommands(
+                new InstantCommand(arm::holyMolyStringingBroke)
+        );
+    }
+}
